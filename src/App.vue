ab4b3ccefee1376a675d90efcbed57be37ad5003
Camilo Alfonso Mosquera Benavides <camiloalfonso.mosquera.contractor@bbva.com>
2024-01-09 15:32:56 -0500

    Using Mapper Helpers With Mutations and Actions

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">  
+    <the-counter></the-counter>  
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 10</button>
+    <change-count></change-count>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCount from './components/ChangeCount.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCount,
+    FavoriteValue,
   },
+  methods:{
+    addOne(){
+      //this.$store.dispach('increase', {value: 10});
+      this.$store.dispatch('increase', { value: 10 });
+    }
+  }
 };
 </script>
 
